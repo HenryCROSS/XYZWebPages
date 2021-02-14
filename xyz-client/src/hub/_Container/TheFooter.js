30902443d77d8d79e072996c2d87414939cdd78d
xmluozp <zhaoping.luo.winnipeg@gmail.com>
2021-02-14 16:22:03 -0600

    footer tewak

--- xyz-client/src/hub/_Container/TheFooter.js
+++ xyz-client/src/hub/_Container/TheFooter.js
@@ -5,12 +5,12 @@ const TheFooter = () => {
   return (
     <CFooter fixed={false}>
       <div>
-        <a href="https://binx.tv" target="_blank" rel="noopener noreferrer">Binx.tv</a>
+        <a href="https://xyzclan.com" target="_blank" rel="noopener noreferrer">XYZ clan</a>
         <span className="ml-1"> &copy; 2020</span>        
       </div>
       <div className="mfs-auto">
         <span className="mr-1">Powered by</span>
-        <a href="https://binx" target="_blank" rel="noopener noreferrer">BinxKeyStore</a>
+        <a href="https://xyzclan.com" target="_blank" rel="noopener noreferrer">XYZ devs</a>
       </div>
     </CFooter>
   )
